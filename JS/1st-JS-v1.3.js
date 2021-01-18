85664ff28ae15fe032cd9a9e5c8842349bd4d927
annihilator29 <39858782+annihilator29@users.noreply.github.com>
2021-01-18 22:25:37 +0545

    Update 1st-JS-v1.3.js

--- JS/1st-JS-v1.3.js
+++ JS/1st-JS-v1.3.js
@@ -7,14 +7,7 @@ function clicked()
 function changeName(name)
 {
 	x=document.getElementById('name');
-	if(name=="aacis"||name=="ashish"||name=="aacis mahaseth"||name=="ashish mahaseth")
-		{
-			x.innerHTML="Re buirwala Randuk";
-		}
-	else if(name=="susata"||name=="sam"||name=="susata adhikari")
-		{
-			x.innerHTML="I love you baby";
-		}
+	
 	else
 		{
 			x.innerHTML= "Hello "+name;
